81b6ebb69efc67fc15781aee6ebfa27f0212c73c
Komal aher <komalaher18@gmail.com>
2024-04-23 21:11:30 -0700

    changes in seed:undo

--- backend/app/seeds/likes.py
+++ backend/app/seeds/likes.py
@@ -24,6 +24,6 @@ def undo_likes():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.likes RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM favorites"))
+        db.session.execute(text("DELETE FROM likes"))
 
     db.session.commit()
