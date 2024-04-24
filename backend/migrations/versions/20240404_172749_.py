2962558962731d7cd0284db71221bdc7652eff35
Komal aher <komalaher18@gmail.com>
2024-04-23 22:18:58 -0700

    changes in migration file

--- backend/migrations/versions/20240404_172749_.py
+++ backend/migrations/versions/20240404_172749_.py
@@ -7,6 +7,9 @@ Create Date: 2024-04-04 17:27:49.195501
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -73,6 +76,13 @@ def upgrade():
     sa.PrimaryKeyConstraint('songId', 'playlistId')
     )
     # ### end Alembic commands ###
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE songs SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE likes SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE playlistsongs SET SCHEMA {SCHEMA};")
+
 
 
 def downgrade():
